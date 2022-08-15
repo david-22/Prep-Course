87366a42b2dadd03de0bf34779437c6bfee1055d
unknown <david.alejandro.esparza.22@gmail.com>
2022-08-15 09:08:21 -0500

    Couple of more excercise done for today

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,8 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  string = "Henry"
+  return string
   
 }
 
@@ -35,24 +37,31 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  addition = x+y
+  return addition
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  resting = x-y
+  return resting
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  multiply = x*y
+  return multiply
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  didive = x/y
+  return didive
   
 }
 
@@ -60,6 +69,12 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (x===y){
+    return true
+  }else{
+    return false
+  }
+
   
 }
 
